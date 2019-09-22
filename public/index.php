1c0fabea646b9c7f0fc41101fdd5ccd323e82dd9
hojinlee <infohojin@gmail.com>
2019-09-22 18:46:25 +0900

    loading 처리 부분 변경

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php
 
 //오토로드
-require_once "../loading.php";
+const JINYLOAD_FILE = __DIR__.DIRECTORY_SEPARATOR."../loading.php";
+if(file_exists(JINYLOAD_FILE)) {
+    require_once JINYLOAD_FILE;
+} else die("cannot load loading files..");
+
 
 /**
  * Http
  * Request, Response
  */
-
 function http_init()
 {
     $req = new \Core\Http\Request;
@@ -19,6 +22,8 @@ list($req, $res) = http_init();
 
 if ($req->isTypeJson()) {
     echo "json 처리동작";
+
+    
 } else {
     echo "웹동작";
 }
